21496ad944212f1b6c9297c79901e01ca30ce976
Maksim Evstafev <emaxiss@gmail.com>
2020-09-15 22:08:18 -0400

    sum of array

--- 8 kyu.js
+++ 8 kyu.js
@@ -349,3 +349,11 @@ function yearDays(year) {
     return `${year} has 365 days`
 }
 
+//Sum Arrays  https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
+function sum (numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum+=numbers[i];
+    }
+    return sum;
+}
