fe97b93fd1bf6409774f99d67f7e3cd82b55d761
Maksim Evstafev <emaxiss@gmail.com>
2020-11-03 21:08:21 -0500

    find the middle element

--- 7_kyu.js
+++ 7_kyu.js
@@ -109,3 +109,9 @@ function arrayMash (array1, array2) {
     }
     return arr;
 }
+
+//Find the middle element  https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/javascript
+var gimme = function (inputArray) {
+    const arr = [...inputArray].sort((a, b) => a - b);
+    return inputArray.indexOf(arr[1]);
+};
