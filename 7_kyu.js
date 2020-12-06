77b164eb26f0e7f7e6e589e2cd96b9119f8ee86b
Maksim Evstafev <emaxiss@gmail.com>
2020-12-05 23:23:13 -0500

    element rotation 6kyu

--- 7_kyu.js
+++ 7_kyu.js
@@ -164,3 +164,4 @@ function last(x){
     return x.split(' ').sort((a, b) => a[a.length - 1] > b[b.length - 1] ? 1 : -1);
 }
 
+//
---
+++ Rotation.js
@@ -0,0 +1,14 @@
+//Create a function named rotate() that accepts a string argument and
+// returns an array of strings with each letter from the input string being rotated to the end.
+
+//https://www.codewars.com/kata/586560a639c5ab3a260000f3/train/javascript
+// All Star Code Challenge #15
+
+function rotate(str){
+    let arr =[];
+    for (let i = 0; i < str.length; i++){
+        str = str.slice(1) + str[0];
+        arr.push(str)
+    }
+    return arr;
+}
