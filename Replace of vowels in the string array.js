543db57521d61d1a2439fbdb696834e91cd8cafd
Maksim Evstafev <emaxiss@gmail.com>
2020-12-27 00:05:13 -0500

    Replace method. replace vowel in the string array

---
+++ Replace of vowels in the string array.js
@@ -0,0 +1,8 @@
+//The Vowel Code   https://www.codewars.com/kata/53697be005f803751e0015aa/train/javascript
+function encode(string) {
+    return string.replace(/[aeiou]/g, el => ' aeiou'.indexOf(el));
+}
+
+function decode(string) {
+    return string.replace(/[1-5]/g, el => ' aeiou'[el]);
+}
